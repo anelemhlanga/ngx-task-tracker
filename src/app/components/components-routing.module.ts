a657ba7cd40cdd53496c70ac016bf02bee168add
Anele Mhlanga <anelenmhlanga@gmail.com>
2022-12-27 18:32:57 +0200

    add components, header and button components

--- src/app/components/components-routing.module.ts
+++ src/app/components/components-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ComponentsComponent } from './components/components/components.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ComponentsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ComponentsRoutingModule { }
+export class ComponentsRoutingModule {}
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ComponentsRoutingModule } from './components-routing.module';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
+import { ComponentsComponent } from './components/components/components.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    ButtonComponent,
+    ComponentsComponent
+  ],
   imports: [
     CommonModule,
     ComponentsRoutingModule
