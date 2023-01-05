6b4ad55aa58b54bcb895fc280ed4729509f2d7a6
Anele Mhlanga <anelenmhlanga@gmail.com>
2023-01-05 02:27:14 +0200

    add header , button and component components

--- src/app/components/components/button/button.component.ts
+++ src/app/components/components/button/button.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css']
+  styleUrls: ['./button.component.css'],
 })
 export class ButtonComponent {
+  public action: string = 'Click';
+  @Input() text: string;
+  @Input() color: string;
+  @Output() buttonClick = new EventEmitter();
 
+  onClick() {
+    this.buttonClick.emit();
+  }
 }
