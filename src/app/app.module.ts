c9164ee2df0f1ab5684933fa0efca62458dac631
Anele Mhlanga <anelenmhlanga@gmail.com>
2023-03-10 23:27:16 +0200

    fix : add Core Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ComponentsModule],
+  imports: [BrowserModule, AppRoutingModule, ComponentsModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent],
 })
